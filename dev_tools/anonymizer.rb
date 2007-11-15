04409c49049558792c0f47eeafbfa500f37af1b9
look <look@e135c355-f531-400e-8cb8-2d0dfcbb2ce7>
2007-11-15 16:54:55 +0000

    added MMS messages I've received from Europe. Also, an anonymizer script to strip email and phone details
    git-svn-id: svn+ssh://rubyforge.org/var/svn/mms2r/trunk@94 e135c355-f531-400e-8cb8-2d0dfcbb2ce7

---
+++ dev_tools/anonymizer.rb
@@ -0,0 +1,20 @@
+#! /usr/bin/env ruby
+
+if ARGV.size != 3
+  puts "Usage: anonymizer.rb message_file phone_number email_address"
+  exit(1)
+end
+
+message_file = ARGV[0]
+phone_number = ARGV[1]
+email_address = ARGV[2]
+
+message = File.read(message_file)
+message.gsub!(phone_number, '5551234')
+message.gsub!(email_address, 'tommy.tutone@example.com')
+
+out = File.open(message_file, "w")
+out.puts message
+out.close
+
+
