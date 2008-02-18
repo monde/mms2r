77fb6af567c1d23de9fa6a66c81a0b1862b5112c
mikemondragon <mikemondragon@e135c355-f531-400e-8cb8-2d0dfcbb2ce7>
2008-02-18 02:25:39 +0000

    support for mms.mycricket.com
    git-svn-id: svn+ssh://rubyforge.org/var/svn/mms2r/trunk@213 e135c355-f531-400e-8cb8-2d0dfcbb2ce7

---
+++ test/test_mms_mycricket_com.rb
@@ -0,0 +1,60 @@
+require File.join(File.dirname(__FILE__), "..", "lib", "mms2r")
+require File.join(File.dirname(__FILE__), "test_helper")
+require 'test/unit'
+require 'rubygems'
+require 'mocha'
+gem 'tmail', '>= 1.2.1'
+require 'tmail'
+
+class TestMmsMycricketCom < Test::Unit::TestCase
+  include MMS2R::TestHelper
+
+  def test_subject
+    # mms.mycricket.com service
+    mail = TMail::Mail.parse(load_mail('mms.mycricket.com-pic.mail').join)
+    mms = MMS2R::Media.new(mail)
+    assert_equal "", mms.subject
+    mms.purge
+  end
+
+  def test_image
+    # mms.mycricket.com service
+    mail = TMail::Mail.parse(load_mail('mms.mycricket.com-pic.mail').join)
+    mms = MMS2R::Media.new(mail)
+
+    assert_equal 1, mms.media.size
+    assert_nil mms.media['text/plain']
+    assert_nil mms.media['text/html']
+    assert_equal 1, mms.media['image/jpeg'].size
+    assert_not_nil mms.media['image/jpeg'].first
+    assert_match(/10-26-07_1739.jpg$/, mms.media['image/jpeg'].first)
+
+    assert_file_size mms.media['image/jpeg'].first, 7249
+
+    mms.purge
+  end
+
+  def test_image_and_text
+    # mms.mycricket.com service
+    mail = TMail::Mail.parse(load_mail('mms.mycricket.com-pic-and-text.mail').join)
+    mms = MMS2R::Media.new(mail)
+
+    assert_equal 2, mms.media.size
+    assert_nil mms.media['text/html']
+    assert_equal 1, mms.media['text/plain'].size
+    assert_equal 1, mms.media['image/jpeg'].size
+    assert_not_nil mms.media['text/plain'].first
+    assert_not_nil mms.media['image/jpeg'].first
+
+    file = mms.media['text/plain'].first
+    assert_equal true, File::exist?(file), "file #{file} does not exist"
+    text = IO.readlines("#{file}").join
+    assert_match(/Hello World/, text)
+
+    assert_match(/02-14-08_2114.jpg$/, mms.media['image/jpeg'].first)
+    assert_file_size mms.media['image/jpeg'].first, 16770
+
+    mms.purge
+  end
+
+end
