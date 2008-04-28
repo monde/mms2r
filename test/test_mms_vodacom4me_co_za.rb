0673443cb8b19d36b06fbb9bbbefbeb7b39b62e7
mikemondragon <mikemondragon@e135c355-f531-400e-8cb8-2d0dfcbb2ce7>
2008-04-28 07:51:27 +0000

    updates to mms.vodacom4me.co.za Vodacom South Africa by Vijay Yellapragada
    git-svn-id: svn+ssh://rubyforge.org/var/svn/mms2r/trunk@238 e135c355-f531-400e-8cb8-2d0dfcbb2ce7

--- test/test_mms_vodacom4me_co_za.rb
+++ test/test_mms_vodacom4me_co_za.rb
@@ -43,4 +43,63 @@ class TestMmsVodacom4meCoZa < Test::Unit::TestCase
     assert_match(/DSC00184\.JPG$/, mms.media['image/jpeg'].first)
     mms.purge
   end
+
+  def test_new_image_and_number
+    mail = TMail::Mail.parse(load_mail('vodacom4me-southafrica-mms-01.mail').join)
+    mms = MMS2R::Media.new(mail)
+    
+    assert_nil mms.media['text/plain']
+    assert_nil mms.media['text/html']
+    assert_equal '+12345678901', mms.number
+
+    assert_not_nil mms.media['image/gif'].first
+    assert_match(/127612345678901h5mmqcMPXEMPXE40mms.vodacom4me.co.za-slide-0-image.gif$/, mms.media['image/gif'].first)
+    mms.purge
+  end
+
+  def test_new_lots_of_images
+    mail = TMail::Mail.parse(load_mail('vodacom4me-southafrica-mms-04.mail').join)
+    mms = MMS2R::Media.new(mail)
+    
+    assert_nil mms.media['text/plain']
+    assert_nil mms.media['text/html']
+    assert_equal '+12345678901', mms.number
+
+    assert_not_nil mms.media['image/gif'].at(0)
+    assert_match(/ZedLandingMedium\.gif$/, mms.media['image/gif'].at(0))
+
+    assert_not_nil mms.media['image/jpeg'].at(0)
+    assert_match(/240x320_thumb\.jpg\.\.jpeg$/, mms.media['image/jpeg'].at(0))
+
+    assert_not_nil mms.media['image/jpeg'].at(1)
+    assert_match(/240x320_thumb\.jpg\.jpeg$/, mms.media['image/jpeg'].at(1))
+
+    assert_not_nil mms.media['image/jpeg'].at(2)
+    assert_match(/wallpaper1940_thumb\.jpg\.jpeg$/, mms.media['image/jpeg'].at(2))
+
+    assert_not_nil mms.media['image/jpeg'].at(3)
+    assert_match(/playboy\.jpg\.jpeg$/, mms.media['image/jpeg'].at(3))
+
+    assert_not_nil mms.media['image/gif'].at(1)
+    assert_match(/h1_170_30\.gif$/, mms.media['image/gif'].at(1))
+
+    assert_not_nil mms.media['image/jpeg'].at(4)
+    assert_match(/banner0\.jpg\.jpeg$/, mms.media['image/jpeg'].at(4))
+
+    assert_not_nil mms.media['image/gif'].at(2)
+    assert_match(/adv_act_list\.gif$/, mms.media['image/gif'].at(2))
+
+    assert_not_nil mms.media['image/gif'].at(3)
+    assert_match(/adv_act_next\.gif$/, mms.media['image/gif'].at(3))
+
+    assert_not_nil mms.media['image/gif'].at(4)
+    assert_match(/adv_nav_back_cat_app_home\.gif$/, mms.media['image/gif'].at(4))
+
+    assert_not_nil mms.media['image/gif'].at(5)
+    assert_match(/adv_tab_mypage\.gif$/, mms.media['image/gif'].at(5))
+
+    assert_not_nil mms.media['image/jpeg'].at(5)
+    assert_match(/creaturecomforts8_240x320_thumb\.jpg\.jpeg/, mms.media['image/jpeg'].at(5))
+    mms.purge
+  end
 end
