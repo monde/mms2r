40008e1d6e263f4367a81fd2e76f62a18d818cbb
mikemondragon <mikemondragon@e135c355-f531-400e-8cb8-2d0dfcbb2ce7>
2008-04-28 07:51:36 +0000

    support for 3 Ireland / mms.3ireland.ie
    git-svn-id: svn+ssh://rubyforge.org/var/svn/mms2r/trunk@242 e135c355-f531-400e-8cb8-2d0dfcbb2ce7

--- README.txt
+++ README.txt
@@ -21,6 +21,7 @@ Suggestions and patches appreciated and welcomed!
 Corpus of carriers currently processed by MMS2R:
 
 * 1nbox/Idea: 1nbox.net
+* 3 Ireland / mms.3ireland.ie
 * Alltel: message.alltel.com
 * AT&T/Cingular/Legacy: mms.att.net, txt.att.net, mmode.com, mms.mycingular.com, 
   cingularme.com
---
+++ test/test_3ireland_ie.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__), "..", "lib", "mms2r")
+require File.join(File.dirname(__FILE__), "test_helper")
+require 'test/unit'
+require 'rubygems'
+require 'mocha'
+gem 'tmail', '>= 1.2.1'
+require 'tmail'
+
+class Test3IrelandIE < Test::Unit::TestCase
+  include MMS2R::TestHelper
+  
+  def test_image_and_text_and_number
+    mail = TMail::Mail.parse(load_mail('3ireland-mms-01.mail').join)
+    mms = MMS2R::Media.new(mail)
+    
+    assert_equal "351234567890", mms.number
+    assert_equal 1, mms.media.size
+    assert_equal 1, mms.media['image/jpeg'].size
+
+    assert_match /Image013\.jpg$/, mms.media['image/jpeg'].first
+    assert_equal 337, File.size(mms.media['image/jpeg'].first)
+
+    mms.purge
+  end
+end
