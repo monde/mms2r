fd46a6a8bc07485eb68d45e5c5b85090e1b2a3c3
Mike Mondragon <mikemondragon@gmail.com>
2008-09-24 01:04:40 -0700

    info2go / Unicel support

--- test/test_unicel_com.rb
+++ test/test_unicel_com.rb
@@ -9,7 +9,7 @@ require 'tmail'
 class TestMmsAlltelCom < Test::Unit::TestCase
   include MMS2R::TestHelper
 
-  def test_subject_number_image
+  def test_subject_number_image_unicel
     mail = TMail::Mail.parse(load_mail('unicel-image-01.mail').join)
     mms = MMS2R::Media.new(mail)
 
@@ -26,6 +26,22 @@ class TestMmsAlltelCom < Test::Unit::TestCase
     file = mms.media['text/plain'][0]
     text = IO.readlines("#{file}").join
     assert_match(/2068675309/, text)
+
+    mms.purge
+  end
+
+  def test_subject_number_image_info2go
+    mail = TMail::Mail.parse(load_mail('info2go-image-01.mail').join)
+    mms = MMS2R::Media.new(mail)
+
+    assert_equal "", mms.subject
+    assert_equal "2068675309", mms.number
+
+    assert_equal 1, mms.media.size
+    assert_equal 1, mms.media['image/jpeg'].size
+
+    image = mms.media['image/jpeg'].detect{|f| /Image047\.jpeg/ =~ f}
+    assert_equal 337, File.size(image) 
 
     mms.purge
   end
