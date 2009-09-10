9270eb808d5a8c0573f1e0860cedcf5881e6f535
Mike Mondragon <mikemondragon@gmail.com>
2009-09-10 12:10:46 -0700

    color up tests if redgreen gem is available

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,16 +2,17 @@ require 'set'
 require 'net/http'
 require 'net/https'
 require 'pp'
+begin require 'redgreen'; rescue LoadError; end
 
 module MMS2R
   module TestHelper
-  
+
     def assert_file_size(file, size)
       assert_not_nil(file, "file was nil")
       assert(File::exist?(file), "file #{file} does not exist")
       assert(File::size(file) == size, "file #{file} is #{File::size(file)} bytes, not #{size} bytes")
     end
-  
+
     def load_mail(file)
       IO.readlines(mail_fixture(file))
     end
