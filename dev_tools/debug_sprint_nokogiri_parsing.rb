2569fabcc1015a48601a56eb6c1596f57e37b1ca
Mike Mondragon <mikemondragon@gmail.com>
2009-12-20 12:23:33 -0800

    updated tool to use Nokogiri

--- dev_tools/debug_sprint_nokogiri_parsing.rb
+++ dev_tools/debug_sprint_nokogiri_parsing.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
-require 'hpricot'
+require 'nokogiri'
 require 'net/http'
+require 'cgi'
 require 'rubygems'
-require 'hpricot'
 require 'pp'
 
 if ARGV[0].nil?
@@ -11,12 +11,12 @@ if ARGV[0].nil?
   exit
 end
 
-doc = open(ARGV[0]) { |f| Hpricot(f) }
+doc = open(ARGV[0]) { |f| Nokogiri(f) }
 
 puts "TITLE: #{doc.at('title').inner_html}"
 
 #phone number is tucked away in the comment in the head
-c = doc.search("/html/head/comment//").last
+c = doc.search("/html/head/comment()").last
 t = c.content.gsub(/\s+/m," ").strip
 number = / name=&quot;MDN&quot;&gt;(\d+)&lt;/.match(t)[1]
 puts "NUMBER: #{number}"
@@ -26,16 +26,14 @@ puts "NUMBER: #{number}"
 text = doc.search("/html/body//pre").first.inner_html
 puts "TEXT: #{text}"
 
-# just see what they say this MMS is it really doesn't
-# mean anything, the content is in paux image with 
-# a RECIPIENT in its URI path
-trs = doc.search("/html/body//tr")
-text = trs[3].search("/td/p/font/b/")
+# just see what they say this MMS is it really doesn't mean anything, the 
+# content is in faux image with a RECIPIENT in its URI path
+text = doc.search("/html/body//tr[2]/td//b")
 case text.text
 when /You have a Video Mail from/
-  puts "it claims to be a video"
+  puts "it claims to be a video: #{text}"
 when /You have a Picture Mail from /
-  puts "it claims to be an image"
+  puts "it claims to be an image: #{text}"
 else
   puts "what is it? #{text.text}"
 end
@@ -59,15 +57,22 @@ puts "there are #{srcs.size} sources to fetch"
 cnt = 0
 srcs.each do |src|
   puts "--"
-  puts "FETCHING:\n  #{src}"
+  puts "FETCHING:\n  #{src.text}"
 
-   url = URI.parse(src)
-   #res = Net::HTTP.get_response(url)
-   agent = "Mozilla/5.0 (X11; U; Minix3 i686 (x86_64); en-US; rv:1.8.1.1) Gecko/20061208 Firefox/2.0.0.1"
-   res = Net::HTTP.start(url.host, url.port) { |http|
-     req = Net::HTTP::Get.new(url.request_uri, {'User-Agent' => agent})
-     http.request(req)
-   }
+  url = URI.parse(CGI.unescapeHTML(src.text))
+  query={}
+  url.query.split('&').each{|a| p=a.split('='); query[p[0]] = p[1]}
+  query.delete_if{|k, v| k == 'limitsize' or k == 'squareoutput' }
+  url.query = query.map{|k,v| "#{k}=#{v}"}.join("&")
+  # sprint is a ghetto, they expect to see &amp; for video request
+  url.query = url.query.gsub(/&/, "&amp;") if @is_video
+
+  #res = Net::HTTP.get_response(url)
+  agent = "Mozilla/5.0 (X11; U; Minix3 i686 (x86_64); en-US; rv:1.8.1.1) Gecko/20061208 Firefox/2.0.0.1"
+  res = Net::HTTP.start(url.host, url.port) { |http|
+    req = Net::HTTP::Get.new(url.request_uri, {'User-Agent' => agent})
+    http.request(req)
+  }
 
   # prep and write a file
   base = /\/RECIPIENT\/([^\/]+)\//.match(src)[1]
